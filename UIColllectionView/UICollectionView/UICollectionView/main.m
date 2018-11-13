4bfadbf56c9929eded0fc2f54cc8f37bfa1cf828
wanghuizhou <wanghuizhou@guazi.com>
2018-11-13 16:55:59 +0800

    添加UICollectionView学习

---
+++ UIColllectionView/UICollectionView/UICollectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UICollectionView
+//
+//  Created by zzg on 2018/11/13.
+//  Copyright © 2018年 zzg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
