12141e82c863c83fca98881659bc41e59cc0ee96
wanghuizhou <wanghuizhou@guazi.com>
2018-12-10 15:07:41 +0800

    添加微信分享SDK的block

---
+++ wechatShare/wechatShare/Pods/Target Support Files/Pods-wechatShare/Pods-wechatShare-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_wechatShare : NSObject
+@end
+@implementation PodsDummy_Pods_wechatShare
+@end
---
+++ wechatShare/wechatShare/wechatShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wechatShare
+//
+//  Created by zzg on 2018/12/10.
+//  Copyright © 2018年 zzg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
