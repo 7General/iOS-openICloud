78cd1d71a1cd53e2b38f83dfaa5865253d5726d3
wanghuizhou <wanghuizhou@guazi.com>
2018-12-12 15:53:32 +0800

    添加ios9的画中画功能

---
+++ PIPVideo/PIPVideo/PIPVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PIPVideo
+//
+//  Created by zzg on 2018/12/12.
+//  Copyright © 2018年 zzg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
