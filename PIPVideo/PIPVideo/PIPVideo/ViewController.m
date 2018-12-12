a782b2214ca7339eb0a384610c05de8b440ae16c
wanghuizhou <wanghuizhou@guazi.com>
2018-12-12 16:33:35 +0800

    上传系统画中画功能呢

--- PIPVideo/PIPVideo/PIPVideo/ViewController.m
+++ PIPVideo/PIPVideo/PIPVideo/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+/* 引入两个框架 */
+#import <AVKit/AVKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()
+@interface ViewController ()<AVPlayerViewControllerDelegate>
 
 @end
 
@@ -16,14 +19,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    AVAudioSession * audioSession = [AVAudioSession sharedInstance];
+    [audioSession setCategory:AVAudioSessionCategoryPlayback error:nil];
+    
+    NSString * path = [[NSBundle mainBundle]pathForResource:@"282" ofType:@"mp4"];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    AVPlayerViewController * play = [[AVPlayerViewController alloc]init];
+    play.delegate = self;
+    play.player = [[AVPlayer alloc]initWithURL:url];
+    [self presentViewController:play animated:YES completion:nil];
+}
+
+- (BOOL)playerViewControllerShouldAutomaticallyDismissAtPictureInPictureStart:(AVPlayerViewController *)playerViewController {
+    return YES;
+}
+
+- (void)playerViewController:(AVPlayerViewController *)playerViewController restoreUserInterfaceForPictureInPictureStopWithCompletionHandler:(void (^)(BOOL))completionHandler {
+    // 在这里，做一些恢复视图的处理，将之前进入PIP，被dismiss掉的playerViewController重新present
+     [self presentViewController:playerViewController animated:YES completion:^{
+        completionHandler(YES);
+    }];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
