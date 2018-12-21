e3758bbcc9fe3643fe50345fbfbcdac02e989c09
wanghuizhou <wanghuizhou@guazi.com>
2018-12-21 14:37:17 +0800

    添加webview

---
+++ WKWebView/WKWebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKWebView
+//
+//  Created by zzg on 2018/12/21.
+//  Copyright © 2018年 zzg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WKWebView/WKWebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WKWebView
+//
+//  Created by zzg on 2018/12/21.
+//  Copyright © 2018年 zzg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
