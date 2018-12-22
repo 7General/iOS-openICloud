c59e5bd8ce66da04185f4c8b991ddf6b824daab8
wanghuizhou <wanghuizhou@guazi.com>
2018-12-22 14:16:21 +0800

    修改文件夹位置

---
+++ UNRecognisedSelector/UNRecognisedSelector/UNRecognisedSelector/Main/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  UNRecognisedSelector
+//
+//  Created by zzg on 2018/12/22.
+//  Copyright © 2018年 zzg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ UNRecognisedSelector/UNRecognisedSelector/UNRecognisedSelector/Main/MainViewController.m
@@ -0,0 +1,37 @@
+//
+//  MainViewController.m
+//  UNRecognisedSelector
+//
+//  Created by zzg on 2018/12/22.
+//  Copyright © 2018年 zzg. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
